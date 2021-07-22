13f07df351d3c595004b25ffbaa7be9e59c6faa2
walid0010 <walidchekir98@gmail.com>
2021-07-22 12:33:32 +0100

    Initial Commit

---
+++ QuizAppIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizAppIOS
+//
+//  Created by chekir walid on 22/7/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
